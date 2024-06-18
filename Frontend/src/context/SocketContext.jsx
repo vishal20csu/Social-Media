1a317c7914132f52ce7df8ca96a94115e0dcea58
Vishal <vishal20csu323@ncuindia.edu>
2024-06-18 23:03:25 +0530

    sockketIo url updated

--- Frontend/src/context/SocketContext.jsx
+++ Frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({children}) => {
 
     useEffect(()=>{
         if(authUser){
-            const socket = io("http://localhost:8000",{
+            const socket = io("https://social-media-nbxp.onrender.com",{
                 query:{
                     userId:authUser._id
                 }
